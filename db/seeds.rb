d93f84b709f8cd166632d692154a837be3ccc1e4
nathan chaney <nathan91chaney@gmail.com>
2022-06-09 14:53:27 +0800

    added image columns and urls to file

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,8 @@ event1 = Event.create!(
   start_time: Time.new(2022, 8, 28, 13, 0, 0),
   end_time: Time.new(2022, 9, 5, 13, 0, 0),
   description: "Burning Man is an event focused on community, art, self-expression, and self-reliance held annually in the western United States",
-  event_url: "https://burningman.org/"
+  event_url: "https://burningman.org/",
+  image_url: "https://www.wallpaperflare.com/static/66/576/958/burning-man-love-desert-gay-wallpaper-preview.jpg"
 )
 
 event2 = Event.create!(
@@ -43,7 +44,8 @@ event2 = Event.create!(
   start_time: Time.new(2022, 4, 2, 19, 0, 0),
   end_time: Time.new(2022, 4, 2, 21, 0, 0),
   description: "Farewell Yellow Brick Road is an ongoing tour by English musician Elton John that began in Allentown, Pennsylvania on 8 September 2018. It is intended to be John's final tour and will consist of more than 300 concerts worldwide",
-  event_url: "https://www.eltonjohn.com/"
+  event_url: "https://www.eltonjohn.com/",
+  image_url: "https://townsquare.media/site/838/files/2021/06/attachment-eltonjohn-finaltour.jpg?w=1080&h=1080&q=75"
 )
 
 event3 = Event.create!(
@@ -56,5 +58,6 @@ event3 = Event.create!(
   start_time: Time.new(2022, 7, 1, 12, 0, 0),
   end_time: Time.new(2022, 7, 7, 23, 0, 0),
   description: "Founded in 1967 by Claude Nobs, the Montreux Jazz Festival has become over the years an unmissable event, generating legendary stories and performances.",
-  event_url: "https://www.montreuxjazzfestival.com/en/"
+  event_url: "https://www.montreuxjazzfestival.com/en/",
+  image_url: "https://www.montreuxjazzfestival.com/wp-content/uploads/2020/01/MITP-1536x1536.jpg"
 )
