1cfa4704a412e9107b9d5a3ec7cf34c5c3678e02
Mack Grissom <mcintyregrissom@gmail.com>
2022-06-09 12:17:53 +0800

    progress on index - need to merge and repull to get login access

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,10 @@
 class EventsController < ApplicationController
+    def index
+        if params["searchq"]
+            redirect_to root_path unless params["searchq"] =~ /(\w|\d)+/
+            @events = Event.where("address iLIKE ?", "%#{params['searchq']}%")
+                         .or(Event.where("title iLIKE ?", "%#{params['searchq']}%"))
+                         .or(Event.where("category iLIKE ?", "%#{params['searchq']}%")).limit(50)
+            end
+    end
 end
