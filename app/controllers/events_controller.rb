c483efc36b5815eec3c638282137ffd971f3797f
Mack Grissom <mcintyregrissom@gmail.com>
2022-06-09 12:38:58 +0800

    events controller updated index/show methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,15 @@
 class EventsController < ApplicationController
     def index
-        if params["searchq"]
+        if params["q"]
             redirect_to root_path unless params["searchq"] =~ /(\w|\d)+/
-            @events = Event.where("address iLIKE ?", "%#{params['searchq']}%")
+            @events = Event.where("address iLIKE ?", "%#{params['q']}%")
                          .or(Event.where("title iLIKE ?", "%#{params['searchq']}%"))
                          .or(Event.where("category iLIKE ?", "%#{params['searchq']}%")).limit(50)
             end
+        # else redirect_to root_path
+    end
+
+    def show
+        @events = Event.find(params[:id])
     end
 end
