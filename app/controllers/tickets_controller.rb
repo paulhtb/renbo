a1d524fd51fe902bec6d22cbaafc10a6e8a6016c
Carlos Babio <carlos.babio@live.com.au>
2022-06-10 15:15:39 +0800

    fixed images

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,7 @@
 class TicketsController < ApplicationController
+  def show
+    @event = Event.find(params[:event_id])
+    @ticket = Ticket.find(params[:id])
+    @tickets = @event.tickets
+  end
 end
