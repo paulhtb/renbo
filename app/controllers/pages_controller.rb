9d723495edad8184c16b5a9d11a695920f70b13a
Carlos Babio <carlos.babio@live.com.au>
2022-06-13 10:55:59 +0800

    work on ticket dropdown

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @events = Event.last(3)
+    @events = Event.first(3)
   end
 end
